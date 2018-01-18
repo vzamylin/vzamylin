4a6dd9d373907499923e0bb6fd088915126ca2ac
Vitaly Zamylin <nzalin@ya.ru>
2018-01-19 02:18:20 +0300

    1.3. JavaDoc. Code convention [#4087][#37903]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
-public class Calculate{
-	public static void main(String[] args){
+/**
+ * Класс Calculate для выполнения вычислений
+ * @author vzamylin
+ * @version 1
+ * @since 19.01.2018
+*/
+public class Calculate {
+	
+	/**
+	 * Основной метод, выполняемый при запуске класса (точка входа)
+	 * @param args передаваемые параметры
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Пакет для выполнения задач по курсу <a href="http://job4j.ru">http://job4j.ru</a>
+ * @author vzamylin
+ * @version 1
+ * @since 19.01.2018
+*/
+package ru.job4j;
