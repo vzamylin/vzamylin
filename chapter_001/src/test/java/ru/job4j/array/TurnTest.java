db7c8133fb0c48dd764c786db109dec4c50464a8
Vitaly Zamylin <nzalin@ya.ru>
2018-03-02 02:35:16 +0300

    5.2. Перевернуть массив. [#4441][#43273]

---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тесты переворачивания массива.
+ * @author vzamylin
+ * @version 1
+ * @since 02.03.2018
+ */
+public class TurnTest {
+
+    /**
+     * Проверка отсутствия переворота пустого массива или массива с одним элементом.
+     */
+    @Test
+    public void whenLengthIsZeroOrOne() {
+        assertThat(new Turn().back(new int[0]), is(new int[0]));
+        assertThat(new Turn().back(new int[]{-2}), is(new int[]{-2}));
+    }
+
+    /**
+     * Проверка переворота массива нечетной размерности (>1).
+     */
+    @Test
+    public void whenLengthIsOdd() {
+        assertThat(new Turn().back(new int[]{-2, 5, -9}), is(new int[]{-9, 5, -2}));
+        assertThat(new Turn().back(new int[]{0, -2, 3, 8, -11}), is(new int[]{-11, 8, 3, -2, 0}));
+    }
+
+    /**
+     * Проверка переворота массива четной размерности (>0).
+     */
+    @Test
+    public void whenLengthIsEven() {
+        assertThat(new Turn().back(new int[]{1, -4}), is(new int[]{-4, 1}));
+        assertThat(new Turn().back(new int[]{-1, 15, 5, -7}), is(new int[]{-7, 5, 15, -1}));
+    }
+}
